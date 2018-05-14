e85b2c7f3770b9ee514c47c1fc541abcd3098476
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-05-14 10:41:06 -0500

    refactored rankings CTRL to use update method. Now updates the database and keeps on runnin

--- server/controllers/rankingsCtrl.js
+++ server/controllers/rankingsCtrl.js
@@ -7,59 +7,61 @@ const app = express();
 const passport = require("passport");
 
 module.exports.scrapeRankings = (req, res, next) => {
+  let promiseArray = [];
   const { BatterSeason, PitcherSeason } = req.app.get("models");
-  axios.get("http://www.espn.com/espn/print?id=22264663").then(
-    response => {
-      if (response.status === 200) {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        let playerArray = [];
-        let statArray = [];
-        $(".mod-content")
-          .find(".last")
-          .each(function(i, elem) {
-            playerArray[i] = {
-              ranking: $(this)
-                .find("td")
-                .eq(0)
-                .text(),
-              status: $(this)
-                .find("td")
-                .eq(1)
-                .text(),
-              name: $(this)
-                .find("td")
-                .eq(2)
-                .text(),
-              team: $(this)
-                .find("td")
-                .eq(3)
-                .text()
-            };
-          });
-        const playerArrayTrimmed = playerArray.slice(0, 300);
-        let promiseArray = [];
-        playerArrayTrimmed.forEach(player => {
-          promiseArray.push(
-            BatterSeason.findOne({
-              where: { name: player.name }
+  axios.get("http://www.espn.com/espn/print?id=22264663").then(response => {
+    if (response.status === 200) {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      let playerArray = [];
+      let statArray = [];
+      $(".mod-content")
+        .find(".last")
+        .each(function(i, elem) {
+          playerArray[i] = {
+            ranking: $(this)
+              .find("td")
+              .eq(0)
+              .text(),
+            status: $(this)
+              .find("td")
+              .eq(1)
+              .text(),
+            name: $(this)
+              .find("td")
+              .eq(2)
+              .text(),
+            team: $(this)
+              .find("td")
+              .eq(3)
+              .text()
+          };
+        });
+      const playerArrayTrimmed = playerArray.slice(0, 300);
+      playerArrayTrimmed.forEach(player => {
+        promiseArray.push(
+          //   BatterSeason.findOne({
+          //     where: { name: player.name }
+          //   })
+          //     .then(batter => {
+          BatterSeason.update({
+            ranking: player.ranking
+          },
+             {
+              where: { name: player.name },
+              returning: true
             })
-              .then(batter => {
-                  console.log(batter);
-                batter.updateAttributes({
-                  ranking: player.ranking
-                });
-              })
-              .catch(err => {
-                next(err);
-              })
-          );
-        });
-        promise.All(promiseArray).then(() => {
-          console.log(promiseArray);
-        });
-      }
-    },
-    error => console.log(err)
-  );
+            .then(comeback => {
+              console.log("YEAHH", comeback);
+            })
+            .catch(err => {
+              next(err);
+            })
+        );
+      });
+      Promise.all(promiseArray).then(() => {
+        
+      });
+    }
+  });
 };
