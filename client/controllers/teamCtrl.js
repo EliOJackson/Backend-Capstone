d7da3995aa36fdfa55df84947515ca59987b1f08
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-05-16 10:52:13 -0500

    links to baseball reference now added to each player on load

--- client/controllers/teamCtrl.js
+++ client/controllers/teamCtrl.js
@@ -26,27 +26,28 @@ angular.module("FantasyBB").controller("TeamCtrl", function ($scope, PlayerFacto
             $scope.loading = true;            
             PlayerFactory.getPlayersToday().then(data => {
               console.log("mydata", data.data);
-              $scope.batters = data.data.batters;
-              $scope.pitchers = data.data.pitchers;
+              $scope.batters = addUrl(data.data.batters);
+              $scope.pitchers = addUrl(data.data.pitchers);
+              console.log("emily is right, I am wrong", $scope.batters, $scope.pitchers);
               $scope.loading = false;              
             });
         }
 
-        $scope.reverse = () => {
-            PlayerFactory.getPlayersSeason().then(data => {
-                let chups = data.data.pitchers;
-                chups.forEach(pitcher => {
-                    let fullName = pitcher.name;
+        let addUrl = (players) => {
+                let playerArray =[];
+                players.forEach(player => {
+                    let fullName = player.name;
                     let splitName = fullName.split(" ");
                     let spliceName = splitName[0].slice(0, 2);
                     let lastName = splitName[1].slice(0, 5);
                     let lastNameFirstLetter = splitName[1].slice(0,1).toLowerCase();
                     let concatName = (lastName + spliceName).toLowerCase();
-                    let url = `www.baseball-reference.com/players/${lastNameFirstLetter}/${concatName}01.shtml`
-                    console.log(url);
+                    player.url = `http://www.baseball-reference.com/players/${lastNameFirstLetter}/${concatName}01.shtml`
+                    playerArray.push(player);
                 })
-            })
-        }
+                return playerArray;
+            };
+        
         // function load() {
         //     PlayerFactory.getPlayersSeason()
         //     .then(data => {
