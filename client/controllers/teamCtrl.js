7735652facd40abe7ff1dbcdcf1947dcfad91cb0
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-05-15 21:21:19 -0500

    now have a loader overlay set up until data is done

--- client/controllers/teamCtrl.js
+++ client/controllers/teamCtrl.js
@@ -14,17 +14,21 @@ angular.module("FantasyBB").controller("TeamCtrl", function ($scope, PlayerFacto
         }
 
         $scope.season = () => {
+            $scope.loading = true;
             PlayerFactory.getPlayersSeason().then(data => {
               console.log("mydata", data.data);
               $scope.batters = data.data.batters;
               $scope.pitchers = data.data.pitchers;
+              $scope.loading = false;
             });
         }
         $scope.today = () => {
+            $scope.loading = true;            
             PlayerFactory.getPlayersToday().then(data => {
               console.log("mydata", data.data);
               $scope.batters = data.data.batters;
               $scope.pitchers = data.data.pitchers;
+              $scope.loading = false;              
             });
         }
         // function load() {
