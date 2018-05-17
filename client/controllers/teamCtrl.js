78a23eed0bcc98f67acb317057ee97a98b23384c
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-05-16 19:51:00 -0500

    set up modal with table and players image. Now, when selecting a batter, a call will be made to create the modal and update the info of thier last 5 games

--- client/controllers/teamCtrl.js
+++ client/controllers/teamCtrl.js
@@ -1,77 +1,88 @@
 "use strict";
 
-angular.module("FantasyBB").controller("TeamCtrl", function ($scope, PlayerFactory) {
-        let currentUserId = null;
+angular
+  .module("FantasyBB")
+  .controller("TeamCtrl", function($scope, PlayerFactory) {
+    let currentUserId = null;
 
-        $scope.$on("handleBroadcast", function (event, user) {
-            $scope.user = user;
-            console.log("Current 11111", $scope.user.username);
-            $scope.currentUser = user;
-        });
-
-        $scope.console = () => {
-            console.log("Emily is cool", $scope.players);
-        }
-
-        $scope.season = () => {
-            $scope.loading = true;
-            PlayerFactory.getPlayersSeason().then(data => {
-              console.log("mydata", data.data);
-              $scope.batters = addUrl(data.data.batters);
-              $scope.pitchers = addUrl(data.data.pitchers);
-              $scope.loading = false;
-            });
-        }
-        $scope.today = () => {
-            $scope.loading = true;            
-            PlayerFactory.getPlayersToday().then(data => {
-              console.log("mydata", data.data);
-              $scope.batters = addUrl(data.data.batters);
-              $scope.pitchers = addUrl(data.data.pitchers);
-              console.log("emily is right, I am wrong", $scope.batters, $scope.pitchers);
-              $scope.loading = false;              
-            });
-        }
-
-        let addUrl = (players) => {
-                let playerArray =[];
-                players.forEach(player => {
-                    let fullName = player.name;
-                    let splitName = fullName.split(" ");
-                    let spliceName = splitName[0].slice(0, 2);
-                    let lastName = splitName[1].slice(0, 5);
-                    let lastNameFirstLetter = splitName[1].slice(0,1).toLowerCase();
-                    let concatName = (lastName + spliceName).toLowerCase();
-                    if (player.ip) {
-                        player.url = `https://www.baseball-reference.com/players/gl.fcgi?id=${concatName}01&t=p&year=2018`;
-                    }
-                    if (player.rbi) {
-                        player.url = `https://www.baseball-reference.com/players/gl.fcgi?id=${concatName}01&t=b&year=2018`;
-                    }
-                    playerArray.push(player);
-                })
-                return playerArray;
-            };
-
-            $scope.loadIndividual = (url) => {
-                console.log(url);
-                PlayerFactory.getPlayerIndividual(url).then(playerData => {
-                    let statsArray = playerData.data;
-                    let lastOne = (statsArray.length - 1);
-                    let firstSlice = statsArray.slice(0, lastOne);
-                    let firstSliceLength = firstSlice.length;
-                    let sliceNumber = (firstSliceLength - 5);
-                    $scope.previousGames = firstSlice.slice(sliceNumber, firstSliceLength);
-                    console.log(secondSlice);
-                })
-            }
-        
-        // function load() {
-        //     PlayerFactory.getPlayersSeason()
-        //     .then(data => {
-        //         console.log("mydata", data.data);
-        //         $scope.batters = data.data.batters;
-        //     })
-        // }
-        // load();
+    $scope.$on("handleBroadcast", function(event, user) {
+      $scope.user = user;
+      console.log("Current 11111", $scope.user.username);
+      $scope.currentUser = user;
     });
+
+    $scope.console = () => {
+      console.log("Emily is cool", $scope.players);
+    };
+
+    $scope.season = () => {
+      $scope.loading = true;
+      PlayerFactory.getPlayersSeason().then(data => {
+        console.log("mydata", data.data);
+        $scope.batters = addUrl(data.data.batters);
+        $scope.pitchers = addUrl(data.data.pitchers);
+        $scope.loading = false;
+      });
+    };
+    $scope.today = () => {
+      $scope.loading = true;
+      PlayerFactory.getPlayersToday().then(data => {
+        console.log("mydata", data.data);
+        $scope.batters = addUrl(data.data.batters);
+        $scope.pitchers = addUrl(data.data.pitchers);
+        console.log(
+          "emily is right, I am wrong",
+          $scope.batters,
+          $scope.pitchers
+        );
+        $scope.loading = false;
+      });
+    };
+
+    let addUrl = players => {
+      let playerArray = [];
+      players.forEach(player => {
+        let fullName = player.name;
+        let splitName = fullName.split(" ");
+        let spliceName = splitName[0].slice(0, 2);
+        let lastName = splitName[1].slice(0, 5);
+        let lastNameFirstLetter = splitName[1].slice(0, 1).toLowerCase();
+        let concatName = (lastName + spliceName).toLowerCase();
+        if (player.ip) {
+          player.url = `https://www.baseball-reference.com/players/gl.fcgi?id=${concatName}01&t=p&year=2018`;
+        }
+        if (player.rbi) {
+          player.url = `https://www.baseball-reference.com/players/gl.fcgi?id=${concatName}01&t=b&year=2018`;
+        }
+        playerArray.push(player);
+      });
+      return playerArray;
+    };
+
+    $scope.loadIndividual = url => {
+      $scope.loading = true;
+      $scope.previousGames = null;
+      $scope.modalImage = null;
+      $scope.modalName = null;
+      PlayerFactory.getPlayerIndividual(url).then(playerData => {
+        let statsArray = playerData.data;
+        let lastOne = statsArray.length - 1;
+        let firstSlice = statsArray.slice(0, lastOne);
+        let firstSliceLength = firstSlice.length;
+        let sliceNumber = firstSliceLength - 5;
+        $scope.previousGames = firstSlice.slice(sliceNumber, firstSliceLength);
+        $scope.modalImage = $scope.previousGames[0].imgUrl;
+        $scope.modalName = $scope.previousGames[0].name;
+        $scope.loading = false;
+      });
+    };
+
+    // function load() {
+    //     PlayerFactory.getPlayersSeason()
+    //     .then(data => {
+    //         console.log("mydata", data.data);
+    //         $scope.batters = data.data.batters;
+    //     })
+    // }
+    // load();
+  });
--- server/controllers/individualCtrl.js
+++ server/controllers/individualCtrl.js
@@ -7,70 +7,74 @@ const app = express();
 const passport = require("passport");
 
 module.exports.scrapeIndBatter = (req, res, next) => {
-    console.log("in the body", req.body.url);
-axios
-  .get(
-    req.body.url
-  )
-  .then(response => {
-    if (response.status === 200) {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      let statArray = [];
-      $("#batting_gamelogs")
-        .find("tr")
-        .each(function(i, elem) {
-          console.log($(this).text());
-          statArray.push({
-            date: $(this)
-              .find("td")
-              .eq(2)
-              .text(),
-            opponent: $(this)
-              .find("td")
-              .eq(5)
-              .text(),
-            result: $(this)
-              .find("td")
-              .eq(6)
-              .text(),
-            ab: $(this)
-              .find("td")
-              .eq(8)
-              .text(),
-            h: $(this)
-              .find("td")
-              .eq(10)
-              .text(),
-            r: $(this)
-              .find("td")
-              .eq(9)
-              .text(),
-            hr: $(this)
-              .find("td")
-              .eq(13)
-              .text(),
-            rbi: $(this)
-              .find("td")
-              .eq(14)
-              .text(),
-            bb: $(this)
-              .find("td")
-              .eq(15)
-              .text(),
-            sb: $(this)
-              .find("td")
-              .eq(24)
-              .text()
+  console.log("in the body", req.body.url);
+  axios
+    .get(req.body.url)
+    .then(response => {
+      if (response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        let statArray = [];
+        $("#batting_gamelogs")
+          .find("tr")
+          .each(function(i, elem) {
+            console.log($(this).text());
+            statArray.push({
+              date: $(this)
+                .find("td")
+                .eq(2)
+                .text(),
+              opponent: $(this)
+                .find("td")
+                .eq(5)
+                .text(),
+              result: $(this)
+                .find("td")
+                .eq(6)
+                .text(),
+              ab: $(this)
+                .find("td")
+                .eq(8)
+                .text(),
+              h: $(this)
+                .find("td")
+                .eq(10)
+                .text(),
+              r: $(this)
+                .find("td")
+                .eq(9)
+                .text(),
+              hr: $(this)
+                .find("td")
+                .eq(13)
+                .text(),
+              rbi: $(this)
+                .find("td")
+                .eq(14)
+                .text(),
+              bb: $(this)
+                .find("td")
+                .eq(15)
+                .text(),
+              sb: $(this)
+                .find("td")
+                .eq(24)
+                .text(),
+              imgUrl: $("#meta")
+                .find("img")
+                .attr("src"),
+              name: $("#meta")
+                .find("h1")
+                .text()
+            });
           });
-        });
-    //   console.log(statArray);
-      res.status(200).json(statArray);
-    }
-  })
-  .catch(err => {
-    next(err);
-  });
+        //   console.log(statArray);
+        res.status(200).json(statArray);
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 
-error => console.log(err);
-}
+  error => console.log(err);
+};
